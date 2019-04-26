b21c16d08334724c7b7c52b84d2eb72c47b8188f
Bernat Moragas <bmoragas@gmail.com>
2019-04-26 10:50:09 +0200

    Position with Market

--- Exercices/Bootstrap/dev/js/leaflet.js
+++ Exercices/Bootstrap/dev/js/leaflet.js
@@ -2,7 +2,6 @@ var mymap = L.map('mapid').setView([41.40113, 2.19601], 13);
 
   // load a tile layer
 
-
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYm1vcmFnYXMiLCJhIjoiY2p1a3ppaGd6MDY5bjN5bzVubGZwNThwMSJ9.o-NVSSmazYTwlhDy6xteuQ', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -10,15 +9,26 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     accessToken: 'pk.eyJ1IjoiYm1vcmFnYXMiLCJhIjoiY2p1a3ppaGd6MDY5bjN5bzVubGZwNThwMSJ9.o-NVSSmazYTwlhDy6xteuQ'
 }).addTo(mymap);
 
-//Marker
 
-//var marker = L.marker([51.5, -0.09]).addTo(map);
-//marker.bindPopup("<b>Hello world!</b><br>Aquí estic.").openPopup();
 
+//Go to searched Location
+function searchResult(latitude, longitude){
+       console.log("searchResult Function: "+latitude, longitude);
+      // mymap.flyTo([latitude, longitude], 12);
+       //var marker = L.marker([latitude, longitude]).addTo(mymap);
+    }
+
+  function SearchClick(latitude, longitude){
+    console.log("hola");
+    mymap.flyTo([latitude, longitude], 12);
+    var marker = L.marker([latitude, longitude]).addTo(mymap);
+  }
+  
 //Click on location
 var popup = L.popup();
 
 function onMapClick(e) {
+
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
--- Exercices/Bootstrap/dev/js/search.js
+++ Exercices/Bootstrap/dev/js/search.js
@@ -1,3 +1,44 @@
+function init() {
+    var input = document.getElementById('LocationSearch');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.setFields(
+        ['address_components', 'geometry', 'icon', 'name']);
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+    autocomplete.addListener('place_changed', function () {
+        infowindow.close();
 
+    var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        };
+        var latitude = place.geometry.location.lat();
+        var longitude = place.geometry.location.lng();
      
-  
+     
+
+    var address = '';
+        if (place.address_components) {
+          address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+        }
+
+
+
+    console.log("Lat: "+ latitude +", long: " + longitude);
+    console.log(address);
+    
+    searchResult(latitude, longitude);    
+    
+    
+})
+}
+
+google.maps.event.addDomListener(window, 'load', init);
